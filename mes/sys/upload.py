f28b10779d3eca44e78aec67a710f4d625ce6d8c
itmanharry <jack_chengqian@163.com>
2023-06-12 13:52:07 +0800

    add upload file function

---
+++ mes/sys/upload.py
@@ -0,0 +1,14 @@
+import os
+def handle_uploaded_file(file=None):
+    if file:
+        # 文件上传路径
+        upload_path = os.path.join(os.path.abspath('.'), 'attachments/files')
+        if not os.path.exists(upload_path):
+            os.makedirs(upload_path)
+        file_to_upload = os.path.join(upload_path, file.name)
+        # 执行上传
+        with open(file_to_upload, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+        return file_to_upload
+    return None
