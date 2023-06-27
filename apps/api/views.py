4543dc39f832b16e07ed34a1e848bd8e372442d5
itmanharry <jack_chengqian@163.com>
2023-06-27 08:41:41 +0800

    token generation and validation finished

--- apps/api/views.py
+++ apps/api/views.py
@@ -26,7 +26,7 @@ def get_token(request):
     user = authenticate(request, username=username, password=password)
     if user:
         payload = {'username': username}
-        token = gen_token(payload)
+        token = gen_token(payload, 10)
         response = JsonResponse({'code': 1, 'message': '令牌获取成功！', 'token': token})
     else:
         response = JsonResponse({'code': 0, 'message': '用户名或密码错误！'})
@@ -36,9 +36,11 @@ def get_token(request):
 @csrf_exempt
 def validate_token(request):
     token = request.POST.get('token')
+    print('Token is : ', token)
     secret_key = settings.SECRET_KEY
     try:
-        result = jwt.decode(token, secret_key)
+        # 解码时也要指定加密算法
+        result = jwt.decode(token, key=secret_key, algorithms=['HS256'])
         print('Result is : ', result)
         response = JsonResponse({'code': 200, 'message': '令牌验证通过!'})
     except exceptions.ExpiredSignatureError:
@@ -47,16 +49,16 @@ def validate_token(request):
         response = JsonResponse({'code': 1002, 'message': '令牌认证失败!'})
     except exceptions.InvalidTokenError:
         response = JsonResponse({'code': 1003, 'message': '非法令牌!'})
-        # 支持跨域访问
+    # 支持跨域访问
     response['Access-Control-Allow-Origin'] = '*'  # 允许所有IP访问
     return response
 def gen_token(payload, timeout=1):
     secret_key = settings.SECRET_KEY
-    headers = {
-        'type': 'jwt',
-        'alg': 'HS256',
-    }
+    # headers = {
+    #     'type': 'jwt',
+    #     'alg': 'HS256',
+    # }
     payload['exp'] = datetime.datetime.utcnow() + datetime.timedelta(minutes=timeout)  # 设置到期时间(默认1分钟)
-    token = jwt.encode(payload=payload, key=secret_key, headers=headers, algorithm='HS256')
+    token = jwt.encode(payload=payload, key=secret_key, algorithm='HS256')
     print('Token type is : ', type(token))
     return token
