bb3053679b2b8d144c83ecd75715b4ebf3edefb9
itmanharry <jack_chengqian@163.com>
2024-03-13 16:33:24 +0800

    add 404 error page for system

--- mes/sys/decorators.py
+++ mes/sys/decorators.py
@@ -12,14 +12,15 @@ def check_menu_used(menu_code):
             if user:
                 menu = Menu.objects.get(code=menu_code)
                 if menu:
-                    role_id = request.session['role_id']
-                    role = Role.objects.get(pk=role_id)
-                    rum = RecentUsedMenu.objects.filter(menu=menu, user=user, role=role).first()
-                    if not rum:
-                        RecentUsedMenu.objects.create(menu=menu, user=user, role=role)
-                    else:
-                        rum.updated_on = timezone.now()
-                        rum.save()
+                    role_id = request.session.get('role_id', None)
+                    if role_id:
+                        role = Role.objects.get(pk=role_id)
+                        rum = RecentUsedMenu.objects.filter(menu=menu, user=user, role=role).first()
+                        if not rum:
+                            RecentUsedMenu.objects.create(menu=menu, user=user, role=role)
+                        else:
+                            rum.updated_on = timezone.now()
+                            rum.save()
             return function(request, *args, **kwargs)
         return decorated_function
     return do_check
